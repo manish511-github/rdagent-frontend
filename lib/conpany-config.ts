d105d217ab047346490d74f1f827fc8dc399c40a
Saurav Sharma <aniketsaurav18@gmail.com>
2025-11-18 22:11:23 +0530

    feat: added support for contact us page

---
+++ lib/conpany-config.ts
@@ -0,0 +1,8 @@
+const COMPANY_DATA = {
+  name: "Zooptics",
+  displayName: "Zooptics.com",
+  supportEmail: "contact@zooptics.com",
+  description: "an AI marketing automation application",
+} as const;
+
+export default COMPANY_DATA;
